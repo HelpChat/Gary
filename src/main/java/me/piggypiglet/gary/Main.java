45dfd2a08d1accce737d5ce568348f0cba6cedcf
PiggyPiglet <noreply@piggypiglet.me>
2018-02-03 20:45:59 +0800

    oof

---
+++ src/main/java/me/piggypiglet/gary/Main.java
@@ -0,0 +1,8 @@
+package me.piggypiglet.gary;
+
+// ------------------------------
+// Copyright (c) PiggyPiglet 2017
+// https://www.piggypiglet.me
+// ------------------------------
+public class Main {
+}
