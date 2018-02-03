e43978622a23c23bb55e8fe6d9ad096979e48fb1
PiggyPiglet <PiggyPiglet@WickedCubeMC.com>
2018-02-03 20:39:10 +0800

    Revert "k no can't use it, bad, need to stick with gitkraken oof"
    This reverts commit be693679c25cb31399a1cd39d46e528f966c3138.

--- src/main/java/me/piggypiglet/gary/Main.java
+++
@@ -1,8 +0,0 @@
-package me.piggypiglet.gary;
-
-// ------------------------------
-// Copyright (c) PiggyPiglet 2017
-// https://www.piggypiglet.me
-// ------------------------------
-public class Main {
-}
