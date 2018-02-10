078fbfe8d74a74fb78bcf62a725a57d49215e6d9
PiggyPiglet <noreply@piggypiglet.me>
2018-02-10 19:04:11 +0800

    Test2

--- src/main/java/me/piggypiglet/gary/core/objects/Constants.java
+++ src/main/java/me/piggypiglet/gary/core/objects/Constants.java
@@ -6,6 +6,6 @@ package me.piggypiglet.gary.core.objects;
 // ------------------------------
 public final class Constants {
 
-    public static final String BOT_TOKEN = "test", AI_TOKEN = "test";
+    public static final String BOT_TOKEN = "test1", AI_TOKEN = "test1";
 
 }
