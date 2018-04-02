bca8a4622bad9c1e12c70bed10ace3728bf5ff35
PiggyPiglet <noreply@piggypiglet.me>
2018-04-02 15:22:59 +0800

    Disabled chatreaction.

--- src/main/java/me/piggypiglet/gary/core/tasks/RunTasks.java
+++ src/main/java/me/piggypiglet/gary/core/tasks/RunTasks.java
@@ -26,11 +26,10 @@ public final class RunTasks {
 
         System.out.println("Task - Request, offer and rms service cleaning started");
         long timeCC = TimeUnit.DAYS.toMillis(Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH));
-//        long time = TimeUnit.SECONDS.toMillis(20);
         timer.schedule(cc, timeCC, timeCC);
 
-        System.out.println("Task - April Fools started");
-        long timeAF = TimeUnit.MINUTES.toMillis(5);
-        timer.schedule(wc, TimeUnit.SECONDS.toMillis(1), timeAF);
+//        System.out.println("Task - April Fools started");
+//        long timeAF = TimeUnit.MINUTES.toMillis(5);
+//        timer.schedule(wc, TimeUnit.SECONDS.toMillis(1), timeAF);
     }
 }
