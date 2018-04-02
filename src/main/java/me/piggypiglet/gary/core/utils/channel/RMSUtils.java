e8113d666244a324abdc2aa78f72f99bbe5dfda5
PiggyPiglet <noreply@piggypiglet.me>
2018-04-02 15:12:24 +0800

    RMS Update to send the users message via pm and hastebin on more error catching areas, also added checks for the website.

--- src/main/java/me/piggypiglet/gary/core/utils/channel/RMSUtils.java
+++ src/main/java/me/piggypiglet/gary/core/utils/channel/RMSUtils.java
@@ -57,41 +57,7 @@ public class RMSUtils {
                 return true;
             } else {
                 message.delete().queue();
-                String requirements = "**Reviews** - The message must start with [REVIEW]\n" +
-                        "**RMS Request** - You must have '[Name]' in your request.\n" +
-                        "**RMS Request** - You must have '[IP]' in your request\n" +
-                        "**RMS Request** - Not required, but we suggest having '[Website]' in your request.\n" +
-                        "**RMS Request** - You must have '[Description]' in your request.";
-                String exampleReview = "[REVIEW]\n" +
-                        "message";
-                String exampleRMS = "[Name] TestPlugins\n" +
-                        "[IP] testplugins.com\n" +
-                        "[Website] https://testplugins.com\n" +
-                        "[Description] Cool description";
-
-                User finalAuthor = author;
-                author.openPrivateChannel().queue(privateChannel -> {
-                    String string = "Your latest request is not following the requirements for <#424460627663126538>.\n" +
-                            "The requirements are as below:\n```" + requirements + "```\nFor example, your review needs to look like this: \n```" + exampleReview + "```\n" +
-                            "RMS requests need to look like this:\n```" + exampleRMS + "```\n" +
-                            "Please edit your message below to fit the requirements:\n```" + msg + "```";
-
-                    privateChannel.sendMessage(string).queue(message1 -> {
-                    }, throwable -> {
-                        String hastebin = wutil.hastebin(string.replace("`", ""));
-
-                        String toSend = !hastebin.equals("fail")
-                                ? "**THIS MESSAGE WILL BE REMOVED IN 30 SECONDS!**\n" + finalAuthor.getAsMention()
-                                + " Your message does not follow the requirements for <#424460627663126538>, "
-                                + "please read this:\n" + hastebin
-                                : "**THIS MESSAGE WILL BE REMOVED IN 30 SECONDS!**\n" + finalAuthor.getAsMention()
-                                + " Your message does not follow the requirements for <#424460627663126538>, "
-                                + "please fix any mistakes.\nhastebin.com is down and you have pm's disabled, "
-                                + "I cannot show you your message, you will have to try remember it.";
-
-                        channel.sendMessage(toSend).queue(message1 -> message1.delete().completeAfter(30, TimeUnit.SECONDS));
-                    });
-                });
+                sendHelp(author, channel, msg);
             }
         }
         return false;
@@ -100,6 +66,7 @@ public class RMSUtils {
     public void createMessage(GenericMessageEvent e) {
         if (checkMessage(e)) {
             Message message = e.getChannel().getMessageById(e.getMessageIdLong()).complete();
+            MessageChannel channel = e.getChannel();
             User author = message.getAuthor();
 
             if (!mutil.startsWith(message.getContentRaw(), "[review]")) {
@@ -131,24 +98,22 @@ public class RMSUtils {
                 }
 
                 String[] ipSegments = ip.split(":");
-                
+
                 switch (ipSegments.length) {
                     case 0:
                         message.delete().queue();
-                        e.getChannel().sendMessage(author.getAsMention() + " You didn't include an address for your server.").complete().delete().queueAfter(30, TimeUnit.SECONDS);
-                        break;
+                        channel.sendMessage(author.getAsMention() + " You didn't include an address for your server.").complete().delete().queueAfter(30, TimeUnit.SECONDS);
+                        sendHelp(author, channel, message.getContentRaw());
+                        return;
                     case 1:
                         ipSegments = (ip + ":25565").split(":");
                         break;
-                    default:
-                        message.delete().queue();
-                        e.getChannel().sendMessage(author.getAsMention() + " The address you entered is invalid, please resend your rms.").complete().delete().queueAfter(30, TimeUnit.SECONDS);
-
                 }
 
                 if (!siutils.serverStatus(ipSegments[0], ipSegments[1])) {
                     message.delete().queue();
-                    e.getChannel().sendMessage(author.getAsMention() + " The server you specified is either offline or doesn't exist, please check the address/turn the server on.").complete().delete().queueAfter(30, TimeUnit.SECONDS);
+                    channel.sendMessage(author.getAsMention() + " The address you specified is unpingable. Check you supplied the correct port/address, and check the server is turned on.").complete().delete().queueAfter(30, TimeUnit.SECONDS);
+                    sendHelp(author, channel, message.getContentRaw());
                     return;
                 }
 
@@ -158,7 +123,7 @@ public class RMSUtils {
                 MessageEmbed.Field nameField = new MessageEmbed.Field("Name:", name, false);
                 MessageEmbed.Field ipField = new MessageEmbed.Field("IP:", ip, false);
                 MessageEmbed.Field descriptionField = new MessageEmbed.Field("Description:", description, false);
-                MessageEmbed.Field extraField = new MessageEmbed.Field("Extra:", extra,true);
+                MessageEmbed.Field extraField = new MessageEmbed.Field("Extra:", extra, true);
 
                 EmbedBuilder newMessage = new EmbedBuilder()
                         .setThumbnail(siutils.getIconURL(ipSegments[0], ipSegments[1]))
@@ -168,16 +133,58 @@ public class RMSUtils {
                         .addField(extraField)
                         .setFooter(author.getName() + "#" + author.getDiscriminator(), message.getAuthor().getAvatarUrl());
 
-                if (!website.equals("")) {
+                if (!website.equals("") && !mutil.startsWith(website, "https:/http:")) {
+                    message.delete().queue();
+                    channel.sendMessage(author.getAsMention() + " the website you've provided is invalid.").complete().delete().queueAfter(30, TimeUnit.SECONDS);
+                    sendHelp(author, channel, message.getContentRaw());
+                    return;
+                } else if (!website.equals("") && mutil.startsWith(website, "https:/http:")) {
                     newMessage.setTitle("Rate My Server", website);
                 } else {
                     newMessage.setTitle("Rate My Server");
                 }
 
-                e.getChannel().sendMessage(newMessage.build()).queue();
+                channel.sendMessage(newMessage.build()).queue();
                 message.delete().queue();
             }
         }
+    }
+
+    private void sendHelp(User author, MessageChannel channel, String msg) {
+        String requirements = "**Reviews** - The message must start with [REVIEW]\n" +
+                "**RMS Request** - You must have '[Name]' in your request.\n" +
+                "**RMS Request** - You must have '[IP]' in your request\n" +
+                "**RMS Request** - Not required, but we suggest having '[Website]' in your request.\n" +
+                "**RMS Request** - You must have '[Description]' in your request.";
+        String exampleReview = "[REVIEW]\n" +
+                "message";
+        String exampleRMS = "[Name] TestPlugins\n" +
+                "[IP] testplugins.com\n" +
+                "[Website] https://testplugins.com\n" +
+                "[Description] Cool description";
+
+        author.openPrivateChannel().queue(privateChannel -> {
+            String string = "Your latest request is not following the requirements for <#424460627663126538>.\n" +
+                    "The requirements are as below:\n```" + requirements + "```\nFor example, your review needs to look like this: \n```" + exampleReview + "```\n" +
+                    "RMS requests need to look like this:\n```" + exampleRMS + "```\n" +
+                    "Please edit your message below to fit the requirements:\n```" + msg + "```";
+
+            privateChannel.sendMessage(string).queue(message1 -> {
+            }, throwable -> {
+                String hastebin = wutil.hastebin(string.replace("`", ""));
+
+                String toSend = !hastebin.equals("fail")
+                        ? "**THIS MESSAGE WILL BE REMOVED IN 30 SECONDS!**\n" + author.getAsMention()
+                        + " Your message does not follow the requirements for <#424460627663126538>, "
+                        + "please read this:\n" + hastebin
+                        : "**THIS MESSAGE WILL BE REMOVED IN 30 SECONDS!**\n" + author.getAsMention()
+                        + " Your message does not follow the requirements for <#424460627663126538>, "
+                        + "please fix any mistakes.\nhastebin.com is down and you have pm's disabled, "
+                        + "I cannot show you your message, you will have to try remember it.";
+
+                channel.sendMessage(toSend).queue(message1 -> message1.delete().completeAfter(30, TimeUnit.SECONDS));
+            });
+        });
     }
 
 }
