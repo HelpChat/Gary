f1ed50a47a9b2045fec610db878633453b85c25d
PiggyPiglet <noreply@piggypiglet.me>
2018-12-12 13:05:08 +0800

    Removed Everything

--- src/main/resources/schema/Stats.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `gary_stats` (
-  `id` smallint(6) NOT NULL AUTO_INCREMENT,
-  `discord_id` bigint(20) NOT NULL,
-  `win` mediumint(9) NOT NULL,
-  `o` mediumint(9) NOT NULL,
-  `bro` mediumint(9) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
