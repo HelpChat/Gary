27e696c91dda691f4de80ff24a2ba8b6e2e6f183
PiggyPiglet <noreply@piggypiglet.me>
2019-01-01 12:15:29 +0800

    Pushed updated constants class

--- src/main/java/me/piggypiglet/gary/core/objects/Constants.java
+++ src/main/java/me/piggypiglet/gary/core/objects/Constants.java
@@ -7,8 +7,10 @@ import java.awt.*;
 // https://www.piggypiglet.me
 // ------------------------------
 public final class Constants {
-    public static final long GARY = 411094432402636802L, LOG = 434652560939286528L;
+    public static final long GARY = 411094432402636802L, LOG = 434652560939286528L, GIVEAWAY = 411094432402636802L;
     public static final String[] CHANNELS = {"411094432402636802", "339674158596358145", "164280494874165248"};
+
+    public static final long SERVICES = 382856487589904395L;
 
     public static final long PIGGYPIGLET = 181675431362035712L;
 
@@ -22,7 +24,7 @@ public final class Constants {
     public static final String[] RATINGS = {"520488319428198400", "520488320946536458", "520488327477198848", "520488323953983489", "520488325253955594"};
     public static final String THUMBSUP = "\uD83D\uDC4D";
 
-    public static final long HELPFUL = 333634764085133313L, TRUSTED = 164528648873836544L, ADMIN = 164525396354793472L, GLOBAL_MUTE = 487901903737454592L, GIVEAWAY = 457243404485525515L;
+    public static final long HELPFUL = 333634764085133313L, TRUSTED = 164528648873836544L, ADMIN = 164525396354793472L, GLOBAL_MUTE = 487901903737454592L, GIVEAWAY_ROLE = 457243404485525515L;
 
     public static final Color GARY_COLOR = new Color(68, 192, 190), RED = new Color(255, 71, 15), BLUE = new Color(17, 126, 166),
             GREEN = new Color(35, 209, 96), YELLOW = new Color(250, 166, 26);
